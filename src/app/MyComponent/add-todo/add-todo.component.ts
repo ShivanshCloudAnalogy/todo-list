49eb90f18030f944ae5d8db69ee75a32ae7c92c6
ShivanshCloudAnalogy <shivansh.shukla@cloudanalogy.com>
2024-03-20 19:44:34 +0530

    todo-list

---
+++ src/app/MyComponent/add-todo/add-todo.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Todo } from '../../Todo';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-add-todo',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './add-todo.component.html',
+  styleUrl: './add-todo.component.css'
+})
+export class AddTodoComponent {
+  title!: string;
+  desc!: string;
+  @Output() todoAdd: EventEmitter<Todo> = new EventEmitter;
+  constructor(){};
+
+  onSubmit(){
+    const todo = {
+      sno: 0,
+      title: this.title,
+      desc: this.desc,
+      active: true
+    }
+    this.todoAdd.emit(todo)
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TodosComponent } from './MyComponent/todos/todos.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TodosComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'Todo-List';
+  // constructor(){
+  //   setTimeout(() => {
+  //     this.title = "Shivansh's Todoslist"
+  //   }, 2000);
+  // }
 }
